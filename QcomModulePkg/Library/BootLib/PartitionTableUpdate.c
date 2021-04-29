33a4df9731a329c5accf3aff043fbdfc4b4690a2
liquan.zhou <liquan.zhou@t2mobile.com>
2021-04-29 11:07:42 +0800

    [1][FP4-215]don't erase data when updating gpt by fastboot
     ###%%%comment:[FP4-215]don't erase data when updating gpt by fastboot
     ###%%%bug number:11060635
     ###%%%product name:sm7225_r_fp4
     ###%%%root cause:Coding
     ###%%%Bug category:T2M
     ###%%%Module_Impact:lk
     ###%%%Test_Suggestion:check
     ###%%%Solution:modify
     ###%%%Test_Report:pass
     ###%%%VAL Can Test:no
    
    Change-Id: Ic56a1a2794e3b13e4151f450c484842ebabf5019

--- QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
+++ QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
@@ -1073,12 +1073,17 @@ WriteGpt (INT32 Lun, UINT32 Sz, UINT8 *Gpt)
     DEBUG ((EFI_D_ERROR, "Failed to patch GPT\n"));
     return Ret;
   }
+
+  //+ FP4-215, don't erase partitions when flash gpt, liquan.zhou.t2m, 20210428
+  #if 0
   /* Erase the entire card */
   Status = ErasePartition (BlockIo, BlockIoHandle[0].Handle);
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Error erasing the storage device: %r\n", Status));
     return FAILURE;
   }
+  #endif
+  //- FP4-215, don't erase partitions when flash gpt, liquan.zhou.t2m, 20210428
 
   /* write the protective MBR */
   Status = BlockIo->WriteBlocks (BlockIo, BlockIo->Media->MediaId, 0, BlkSz,
