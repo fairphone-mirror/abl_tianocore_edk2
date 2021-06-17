07ce97900bdbe998d84aee54273f27663be4b1eb
liquan.zhou <liquan.zhou@t2mobile.com>
2021-06-17 19:49:28 +0800

    [1][FP4-851] The text of the guidance message is not completely
     ###%%%comment:The text of the guidance message is not completely
     ###%%%bug number:FP4-851
     ###%%%product name:sm7225_r_fp4
     ###%%%root cause:Coding
     ###%%%Bug category:T2M
     ###%%%Module_Impact:abl
     ###%%%Test_Suggestion:retest
     ###%%%Solution:modify
     ###%%%Test_Report:pass
     ###%%%VAL Can Test:yes
    
    Change-Id: Ia20f3857542181a0876632bd0251f77776b04b27

--- QcomModulePkg/Library/BootLib/FastbootMenu.c
+++ QcomModulePkg/Library/BootLib/FastbootMenu.c
@@ -165,7 +165,7 @@ EFI_STATUS
 UpdateFastbootOptionItem (UINT32 OptionItem, UINT32 *pLocation)
 {
   EFI_STATUS Status = EFI_SUCCESS;
-  UINT32 Location = 0;
+  UINT32 Location = 45; //FP4-851, liquan.zhou.t2m, 20210617, The text of the guidance message is not completely
   UINT32 Height = 0;
   MENU_MSG_INFO *FastbootLineInfo = NULL;
 
