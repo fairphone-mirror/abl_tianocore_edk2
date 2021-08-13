a12302512beecbfb14ad697fd499e9d72b3ac0dd
liquan.zhou <liquan.zhou@t2mobile.com>
2021-08-13 17:58:05 +0800

    [1][FP4-2479]add temp unlock function in bootloader
     ###%%%comment:add temp unlock function in bootloader
     ###%%%bug number:FP4-2479
     ###%%%product name:sm7225_r_fp4
     ###%%%root cause:Coding
     ###%%%Bug category:T2M
     ###%%%Module_Impact:bootloader
     ###%%%Test_Suggestion:fastboot oem allow-flash true
     ###%%%Solution:modify
     ###%%%Test_Report:pass
     ###%%%VAL Can Test:yes
    
    Change-Id: I80aa1fa47000f810efda4afe2b369602001e2a74

--- QcomModulePkg/Library/FastbootLib/FastbootCmds.c
+++ QcomModulePkg/Library/FastbootLib/FastbootCmds.c
@@ -192,6 +192,9 @@ STATIC BOOLEAN LunSet;
 STATIC FASTBOOT_CMD *cmdlist;
 STATIC UINT32 IsAllowUnlock;
 
+//FP4-2479, add debug function in bootloader for user release, liquan.zhou.t2m, 20210813
+STATIC BOOLEAN IsAllowFlashOem = FALSE;
+
 STATIC EFI_STATUS
 FastbootCommandSetup (VOID *Base, UINT64 Size);
 STATIC VOID
@@ -1705,7 +1708,7 @@ CmdFlash (IN CONST CHAR8 *arg, IN VOID *data, IN UINT32 sz)
   if ((GetAVBVersion () == AVB_LE) ||
       ((GetAVBVersion () != AVB_LE) &&
       (TargetBuildVariantUser ()))) {
-    if (!IsUnlocked ()) {
+    if (!IsUnlocked () && !IsAllowFlashOem) {
       FastbootFail ("Flashing is not allowed in Lock State");
       return;
     }
@@ -1978,7 +1981,7 @@ CmdErase (IN CONST CHAR8 *arg, IN VOID *data, IN UINT32 sz)
   if ((GetAVBVersion () == AVB_LE) ||
       ((GetAVBVersion () != AVB_LE) &&
       (TargetBuildVariantUser ()))) {
-    if (!IsUnlocked ()) {
+    if (!IsUnlocked () && !IsAllowFlashOem) {
       FastbootFail ("Erase is not allowed in Lock State");
       return;
     }
@@ -2075,7 +2078,7 @@ CmdSetActive (CONST CHAR8 *Arg, VOID *Data, UINT32 Size)
   Slot NewSlot = {{0}};
   EFI_STATUS Status;
 
-  if (TargetBuildVariantUser () && !IsUnlocked ()) {
+  if (TargetBuildVariantUser () && !IsUnlocked () && !IsAllowFlashOem) {
     FastbootFail ("Slot Change is not allowed in Lock State\n");
     return;
   }
@@ -2560,6 +2563,37 @@ CmdReboot (IN CONST CHAR8 *arg, IN VOID *data, IN UINT32 sz)
   // Shouldn't get here
   FastbootFail ("Failed to reboot");
 }
+
+//+FP4-2479, add debug function in bootloader for user release, liquan.zhou.t2m, 20210813
+STATIC VOID
+CmdOemAllowFlash(IN CONST CHAR8 *Arg, IN VOID *Data, IN UINT32 Size)
+{
+    if(!(strncmp(Arg, " true", 5))) {
+        IsAllowFlashOem = TRUE;
+        FastbootOkay("");
+        return;
+    }
+    FastbootFail ("Failed to allow flash");
+}
+
+STATIC VOID
+CmdClearRollbackIndex (IN CONST CHAR8 *Arg, IN VOID *Data, IN UINT32 Size)
+{
+  EFI_STATUS Status = EFI_SUCCESS;
+
+  Status = WriteRollbackIndex(0,0);
+  if (Status != EFI_SUCCESS) {
+    FastbootFail ("Failed to clear RollbackIndex");
+    return;
+  }
+  Status = WriteRollbackIndex(1,0);
+  if (Status != EFI_SUCCESS) {
+    FastbootFail ("Failed to clear RollbackIndex");
+    return;
+  }
+  FastbootOkay ("");
+}
+//-FP4-2479, add debug function in bootloader for user release, liquan.zhou.t2m, 20210813
 
 //+FP4-492, root for user, liquan.zhou.t2m, 20210531
 STATIC VOID
@@ -3705,6 +3739,10 @@ FastbootCommandSetup (IN VOID *Base, IN UINT64 Size)
       {"flashing get_unlock_ability", CmdFlashingGetUnlockAbility},
       {"flashing unlock", CmdFlashingUnlock},
       {"flashing lock", CmdFlashingLock},
+      //+FP4-2479, add debug function in bootloader for user release, liquan.zhou.t2m, 20210813
+      {"oem allow-flash", CmdOemAllowFlash},
+      {"oem clear-rollback-index", CmdClearRollbackIndex},
+      //-FP4-2479
 #endif
 /*
  *CAUTION(CRITICAL): Enabling these commands will allow changes to bootimage.
