ea845ba46d9c819a15eb9ac504b63611885bcd6a
liquan.zhou <liquan.zhou@t2mobile.com>
2023-02-12 14:18:31 +0800

    [FP5-291] abl: don't erase data when updating gpt by fastboot
    Test: check command fastboot flash partition:5 gpt_both5.bin
    
    Change-Id: I48e350e9df958a93457d4088a622113c3e545141

--- QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
+++ QcomModulePkg/Library/BootLib/PartitionTableUpdate.c
@@ -1073,12 +1073,17 @@ WriteGpt (INT32 Lun, UINT32 Sz, UINT8 *Gpt)
     DEBUG ((EFI_D_ERROR, "Failed to patch GPT\n"));
     return Ret;
   }
+
+  //+ FP5-291, don't erase partitions when flash gpt, liquan.zhou.t2m, 20230212
+  #if 0
   /* Erase the entire card */
   Status = ErasePartition (BlockIo, BlockIoHandle[0].Handle);
   if (Status != EFI_SUCCESS) {
     DEBUG ((EFI_D_ERROR, "Error erasing the storage device: %r\n", Status));
     return FAILURE;
   }
+  #endif
+  //- FP5-291, don't erase partitions when flash gpt, liquan.zhou.t2m, 20230212
 
   /* write the protective MBR */
   Status = BlockIo->WriteBlocks (BlockIo, BlockIo->Media->MediaId, 0, BlkSz,
